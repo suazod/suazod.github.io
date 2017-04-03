dac7d0a41375c7f603fc6e4ca4aaada181fba9a5
Danny Suazo <dannysuazor@yahoo.com>
2017-04-03 00:11:25 -0500

    Add files via upload

--- week-4-game/assets/javascript/game.js
+++ week-4-game/assets/javascript/game.js
@@ -1,28 +1,26 @@
 $(document).ready(function() {
-var wins = 0;
-var losses = 0;
-var counter = 0;
 
+	crystals = ['assets/images/red.png','assets/images/blue.png','assets/images/yellow.png','assets/images/green.png'];
 
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
 
+function createCrystal {
+  var crystalsNumber = []
+  var randomNumber = Math.floor(Math.random()*12) {
+  for (var i=0; i < crystalsNumber.length; i++){
+  if (crystalsNumber[i] == randomNumber){
+  }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-};
+}
+for (i = 0; i < crystalsNumber.length; i++) {
+  var imgCrystal = $('<img>');
+  imageCrystal.attr('data-num', crystalsNumber[i]);
+  imageCrystal.attr('src', crystals[i]);
+  imageCrystal.attr('alt', 'crystals');
+  imageCrystal.addClass('crystalImage');
+  $('#crystals').append(imgCrystal);
+}
+});
