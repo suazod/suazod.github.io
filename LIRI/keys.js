c2946bd72df5e7ae8cd37c14dd634b3ae0553859
Danny Suazo <dannysuazor@yahoo.com>
2017-05-30 01:56:05 -0500

    Delete keys.js

--- LIRI/keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '3lYKJFI19yA4w7GgkDfbKhfNK',
-  consumer_secret: 'GqvrZJjUtMv7o3yGh0dY4jLrYjevHMg0oPw1jz9Nd7mpHPc4hK',
-  access_token_key: '863961104355119108-YEdYPcM2yubdY7Bdkbe653fDQkHwOVo',
-  access_token_secret: 'ar87Ee5Jpj8R83OUel4gtrvmgTyy0NE9C7FAddkz5mIEn',
-}
